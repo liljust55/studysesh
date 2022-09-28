c36895bfbc0011ba82767bbb79ddb9e0ec63794d
Heinson Fernandez <vtsunayushi@gmail.com>
2022-09-28 18:59:05 +0800

    initial commit

---
+++ misc/js/index.js
@@ -0,0 +1,23 @@
+function showMe() {
+    // let a = document.getElementById("floatingInput").value;
+
+
+    // console.log('clicked!');
+    // console.log(a);
+    // alert("it worked");
+
+    let a = document.getElementById('floatingInput').value;
+    let b = document.getElementById('floatingPassword').value
+
+    console.log(a);
+    console.log(b);
+
+
+
+
+}
+
+
+function seeAllInput() {
+    console.log('it works!')
+}
